bbb4381d25e52d25d1145e73d2a86261dce83b4a
teky1 <joelchem@outlook.com>
2023-09-12 17:15:45 -0400

    loaded centerstage field in meepmeep; visualized possible auto path

--- MeepMeepVisualization/src/main/java/com/example/meepmeepvisualization/MeepMeepVisualization.java
+++ MeepMeepVisualization/src/main/java/com/example/meepmeepvisualization/MeepMeepVisualization.java
@@ -1,34 +1,44 @@
 package com.example.meepmeepvisualization;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepVisualization {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(500);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, 60, -Math.PI/2))
+                                .lineToSplineHeading(new Pose2d(-36, 12, Math.PI/2))
+                                .waitSeconds(1)
+                                .lineToSplineHeading(new Pose2d(24, 12, 0))
+                                .splineToConstantHeading(new Vector2d(48, 32), 0)
                                 .build()
                 );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
+        try {
+            BufferedImage image = ImageIO.read(new URL("https://i.imgur.com/g9GR5fM.png"));
+            meepMeep.setBackground(image)
+                    .setDarkMode(true)
+                    .addEntity(myBot)
+                    .start();
+        } catch (IOException e) {
+            System.out.println("Loading background image failed.");
+
+        }
     }
 }
