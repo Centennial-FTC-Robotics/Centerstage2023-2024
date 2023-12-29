4f632bbd2e03772b2a221dca345d22ac929a4a64
Teky1 <officialteky@gmail.com>
2023-12-29 15:35:35 -0500

    auto 1

--- MeepMeepVisualization/src/main/java/com/example/meepmeepvisualization/MeepMeepVisualization.java
+++ MeepMeepVisualization/src/main/java/com/example/meepmeepvisualization/MeepMeepVisualization.java
@@ -15,14 +15,15 @@ import java.util.Objects;
 import javax.imageio.ImageIO;
 
 public class MeepMeepVisualization {
-    public static void main(String[] args) {
+
+    public static void traj1() {
         MeepMeep meepMeep = new MeepMeep(700);
 
-        double tile = 23.75;
+        double tile = 23.5;
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setConstraints(45, 45, 3.8, 3.8, 18)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-1.5*tile, 2.5*tile, 3*Math.PI/2))
                                 .lineToSplineHeading(new Pose2d(-1.5*tile, 1.25*tile, 0))
@@ -44,5 +45,55 @@ public class MeepMeepVisualization {
             System.out.println("Loading background image failed.");
 
         }
+    }
+
+    public static void traj2() {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        double t = 23.5;
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(45, 45, 3.8, 3.8, 18)
+                .followTrajectorySequence(drive ->
+//                        drive.trajectorySequenceBuilder(new Pose2d(-1.5*t, 2.5*t, Math.toRadians(-90)))
+//                                .splineToSplineHeading(new Pose2d(-1.9*t, 1.6*t, Math.toRadians(-100)), Math.toRadians(-100))
+//                                .waitSeconds(0.5)
+//                                .splineToSplineHeading(new Pose2d(-1.5*t, 1.5*t, Math.toRadians(-180)), Math.toRadians(-90))
+//                                .splineToConstantHeading(new Vector2d(-2.5*t, 0.5*t), Math.toRadians(180))
+//                                .build()
+                        drive.trajectorySequenceBuilder(new Pose2d(-1.5*t, 2.5*t, Math.toRadians(270)))
+                                .splineToConstantHeading(new Vector2d(-47.45, 37.48), Math.toRadians(270.00))
+                                .waitSeconds(.5)
+                                .setReversed(true)
+                                .splineToConstantHeading(new Vector2d(-35.46, 36.76), Math.toRadians(270.00))
+                                .setReversed(false)
+                                .splineTo(new Vector2d(-43.69, 16.54), Math.toRadians(220.60))
+                                .splineTo(new Vector2d(-62.03, 11.05), Math.toRadians(180.00))
+                                .waitSeconds(.5)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(24.77, 12.49), Math.toRadians(1.49))
+                                .splineTo(new Vector2d(49.47, 41.67), Math.toRadians(0.00))
+                                .setReversed(false)
+                                .waitSeconds(.5)
+
+                                .build()
+                );
+
+        try {
+            BufferedImage image = ImageIO.read(new URL("https://i.imgur.com/EOT4Pcj.png"));
+            meepMeep.setBackground(image)
+//            meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                    .setDarkMode(false)
+                    .addEntity(myBot)
+                    .start();
+        } catch (IOException e) {
+            System.out.println("Loading background image failed.");
+
+        }
+    }
+
+    public static void main(String[] args) {
+       traj2();
     }
 }
